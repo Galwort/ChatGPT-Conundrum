906c7b52e442ab45ef4b4e2caa709af663599913
Tom Gorbett <gorbetw@ccf.org>
2023-02-23 18:31:59 -0500

    legend replacement

--- Code/pubmed-gpt-viz.py
+++ Code/pubmed-gpt-viz.py
@@ -18,9 +18,28 @@ colors = [
 
 # by year
 yr_df_sample = read_csv(data_dir + "year-sample.csv")
-yr_grouped = (
-    yr_df_sample.groupby(["year_range", "Legend"]).size().unstack()
-).reset_index()
+
+leg_rep = {
+    "Over 50% chance of being real": "Less than 50% chance of being AI generated",
+    "Less than 20% chance of being real": "Greater than 80% chance of being AI generated",
+    "Less than 10% chance of being real": "Greater than 90% chance of being AI generated",
+    "Less than 50% chance of being real": "Greater than 50% chance of being AI generated",
+    "Less than 30% chance of being real": "Greater than 70% chance of being AI generated",
+    "Less than 40% chance of being real": "Greater than 60% chance of being AI generated",
+}
+yr_df_sample["Legend"] = yr_df_sample["Legend"].replace(leg_rep)
+yr_grouped = yr_df_sample.groupby(["year_range", "Legend"]).size().unstack()
+
+new_order = [
+    "Greater than 90% chance of being AI generated",
+    "Greater than 80% chance of being AI generated",
+    "Greater than 70% chance of being AI generated",
+    "Greater than 60% chance of being AI generated",
+    "Greater than 50% chance of being AI generated",
+    "Less than 50% chance of being AI generated",
+]
+yr_grouped = yr_grouped.reindex(columns=new_order)
+
 tick_labels = yr_grouped["year_range"].astype(str)
 yr_grouped.plot(kind="bar", stacked=True, color=colors, figsize=(20, 10))
 
@@ -55,9 +74,18 @@ for i in range(yr_grouped.shape[1]-1):
 plt.savefig(viz_dir + "Years-3D.png")
 
 # by journal
-jr_grouped = (
-    yr_df_sample.groupby(["journal", "Legend"]).size().unstack()
-).reset_index()
+jr_grouped = yr_df_sample.groupby(["journal", "Legend"]).size().unstack()
+
+new_order = [
+    "Greater than 90% chance of being AI generated",
+    "Greater than 80% chance of being AI generated",
+    "Greater than 70% chance of being AI generated",
+    "Greater than 60% chance of being AI generated",
+    "Greater than 50% chance of being AI generated",
+    "Less than 50% chance of being AI generated",
+]
+
+yr_grouped = yr_grouped.reindex(columns=new_order)
 tick_labels = jr_grouped["journal"].astype(str)
 jr_grouped.plot(kind="bar", stacked=True, color=colors, figsize=(20, 10))
 
--- Scrap/abstracts-analysis.py
+++ Scrap/abstracts-analysis.py
@@ -1,52 +1,48 @@
-from pandas import read_csv, set_option
-from os import getcwd
 import matplotlib.pyplot as plt
-
-# set_option('display.width', None)
-
-colors = [
-    "#1f77b4", # blue
-    "#ff7f0e", # orange
-    "#2ca02c", # green
-    "#d62728", # red
-    "#9467bd", # purple
-    "#a5a5a5" # grey
-]
+from pandas import read_csv
+from os import getcwd
 
 dir = getcwd()
 data_dir = dir.replace("Scrap", "Data\\")
-abs_df = read_csv(data_dir + "abstracts.csv")
+
+colors = [
+    "#1f77b4",  # blue
+    "#ff7f0e",  # orange
+    "#2ca02c",  # green
+    "#d62728",  # red
+    "#9467bd",  # purple
+    "#a5a5a5",  # grey
+]
+
+# by year
 yr_df_sample = read_csv(data_dir + "year-sample.csv")
 
-# by segment
-abs_df = read_csv(data_dir + "abstracts.csv")
-seg_df_sample = abs_df.merge(
-    yr_df_sample[["article_url", "year_range", "url"]],
-    on=["article_url", "year_range", "url"],
-    how="inner",
-)
-seg_grouped = (
-    seg_df_sample.groupby(["segment", "Legend"]).size().unstack()
-).reset_index()
-seg_grouped["segment"] = seg_grouped["segment"].astype(str)
+leg_rep = {
+    "Over 50% chance of being real": "Less than 50% chance of being AI generated",
+    "Less than 20% chance of being real": "Greater than 80% chance of being AI generated",
+    "Less than 10% chance of being real": "Greater than 90% chance of being AI generated",
+    "Less than 50% chance of being real": "Greater than 50% chance of being AI generated",
+    "Less than 30% chance of being real": "Greater than 70% chance of being AI generated",
+    "Less than 40% chance of being real": "Greater than 60% chance of being AI generated",
+}
+yr_df_sample["Legend"] = yr_df_sample["Legend"].replace(leg_rep)
+yr_grouped = yr_df_sample.groupby(["year_range", "Legend"]).size().unstack()
 
-# Print the value counts of the "Legend" column
-print(seg_grouped)
+new_order = [
+    "Greater than 90% chance of being AI generated",
+    "Greater than 80% chance of being AI generated",
+    "Greater than 70% chance of being AI generated",
+    "Greater than 60% chance of being AI generated",
+    "Greater than 50% chance of being AI generated",
+    "Less than 50% chance of being AI generated",
+]
+yr_grouped = yr_grouped.reindex(columns=new_order)
 
-tick_labels = seg_grouped["segment"].astype(str)
-seg_grouped.plot(kind="bar", stacked=True, color=colors, figsize=(20, 10))
+tick_labels = yr_grouped.index.astype(str)
+yr_grouped.plot(kind="bar", stacked=True, color=colors, figsize=(20, 10))
 
-plt.xlabel("Segment")
+plt.xlabel("Year Range")
 plt.xticks(range(len(tick_labels)), tick_labels, rotation=0)
 plt.ylabel("Number of Abstracts")
 plt.title("Real published extracts evaluated by AI generated text detector")
 plt.show()
-
-# by journal
-jr_grouped = (
-    yr_df_sample.groupby(["journal", "Legend"]).size().unstack()
-).reset_index()
-tick_labels = jr_grouped["journal"].astype(str)
-jr_grouped.plot(kind="bar", stacked=True, color=colors, figsize=(20, 10))
-
-print(jr_grouped)
