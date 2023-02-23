855eb61d3824efe23c556fc99eb07e090d415f13
Tom Gorbett <gorbetw@ccf.org>
2023-02-23 09:09:54 -0500

    adjusting segment sample formatting

--- Code/pubmed-gpt-pivots.py
+++ Code/pubmed-gpt-pivots.py
@@ -2,16 +2,18 @@ from pandas import read_csv, DataFrame, concat
 from os import getcwd
 from scipy import stats
 
-# testing auto sync
-
 dir = getcwd()
 data_dir = dir.replace("Code", "Data\\")
 tbl_dir = dir.replace("Code", "Tables\\")
 
 # importing sample data
 yr_df_sample = read_csv(data_dir + "year-sample.csv")
-# jr_df_sample = read_csv(data_dir + "journal-sample.csv")
 abs_df = read_csv(data_dir + "abstracts.csv")
+seg_df_sample = abs_df.merge(
+    yr_df_sample[["article_url", "year_range", "url"]],
+    on=["article_url", "year_range", "url"],
+    how="inner",
+)
 
 # creating pivots
 yr_df_sample.pivot_table(index="year_range", columns="Legend", aggfunc="size").to_csv(
@@ -20,12 +22,79 @@ yr_df_sample.pivot_table(index="year_range", columns="Legend", aggfunc="size").t
 yr_df_sample.pivot_table(index="journal", columns="Legend", aggfunc="size").to_csv(
     tbl_dir + "journal-pivot.csv"
 )
-yr_df_sample.pivot_table(index="segment", columns="Legend", aggfunc="size").to_csv(
+seg_df_sample.pivot_table(index="segment", columns="Legend", aggfunc="size").to_csv(
     tbl_dir + "segment-pivot.csv"
 )
 
-# big table for word count and p-value
-bt_df = DataFrame(columns=["year_range","journal","word_count","p_value"])
+# big table for word count and p-value, on sample dataset
+bts_df = DataFrame(columns=["year_range", "journal", "word_count", "p_value"])
+
+for jr in yr_df_sample["journal"].unique():
+    for yr in yr_df_sample["year_range"].unique():
+        ptr_df = yr_df_sample.loc[
+            (yr_df_sample["journal"] == jr) & (yr_df_sample["year_range"] == yr)
+        ]
+        otr_df = yr_df_sample.loc[
+            (yr_df_sample["journal"] != jr) & (yr_df_sample["year_range"] != yr)
+        ]
+        wc = ptr_df["word_count"].mean()
+        _, pv = stats.ttest_ind(
+            ptr_df["word_count"], otr_df["word_count"], equal_var=False
+        )
+        x_df = DataFrame(
+            {"year_range": yr, "journal": jr, "word_count": wc, "p_value": pv},
+            index=[0],
+        )
+        bts_df = concat([bts_df, x_df], ignore_index=True)
+
+yr_tot_df = DataFrame(columns=["year_range", "journal", "word_count", "p_value"])
+for yr in yr_df_sample["year_range"].unique():
+    ptr_df = yr_df_sample.loc[yr_df_sample["year_range"] == yr]
+    otr_df = yr_df_sample.loc[yr_df_sample["year_range"] != yr]
+    wc = ptr_df["word_count"].mean()
+    _, pv = stats.ttest_ind(ptr_df["word_count"], otr_df["word_count"], equal_var=False)
+    x_df = DataFrame(
+        {"year_range": yr, "journal": "Total", "word_count": wc, "p_value": pv},
+        index=[0],
+    )
+    yr_tot_df = concat([yr_tot_df, x_df], ignore_index=True)
+bts_df = concat([bts_df, yr_tot_df], ignore_index=True)
+
+jr_tot_df = DataFrame(columns=["year_range", "journal", "word_count", "p_value"])
+for jr in yr_df_sample["journal"].unique():
+    ptr_df = yr_df_sample.loc[yr_df_sample["journal"] == jr]
+    otr_df = yr_df_sample.loc[yr_df_sample["journal"] != jr]
+    wc = ptr_df["word_count"].mean()
+    _, pv = stats.ttest_ind(ptr_df["word_count"], otr_df["word_count"], equal_var=False)
+    x_df = DataFrame(
+        {"year_range": "Total", "journal": jr, "word_count": wc, "p_value": pv},
+        index=[0],
+    )
+    jr_tot_df = concat([jr_tot_df, x_df], ignore_index=True)
+bts_df = concat([bts_df, jr_tot_df], ignore_index=True)
+
+ptr_df = yr_df_sample
+otr_df = yr_df_sample
+wc = ptr_df["word_count"].mean()
+_, pv = stats.ttest_ind(ptr_df["word_count"], otr_df["word_count"], equal_var=False)
+tot_df = DataFrame(
+    {"year_range": "Total", "journal": "Total", "word_count": wc, "p_value": pv},
+    index=[0],
+)
+bts_df = concat([bts_df, tot_df], ignore_index=True)
+
+bt_pivot = bts_df.pivot_table(
+    index="year_range", columns="journal", values=["word_count", "p_value"]
+)
+bt_pivot = bt_pivot.round(4)
+bt_pivot.columns = [f"{k} {j}" for j, k in bt_pivot.columns]
+journals = sorted(bts_df["journal"].unique())
+column_order = [f"{j} {col}" for j in journals for col in ["word_count", "p_value"]]
+bt_pivot = bt_pivot.reindex(columns=column_order)
+bt_pivot.to_csv(tbl_dir + "statistics-pivot-sample.csv")
+
+# big table for word count and p-value, on entire dataset
+bt_df = DataFrame(columns=["year_range", "journal", "word_count", "p_value"])
 
 for jr in abs_df["journal"].unique():
     for yr in abs_df["year_range"].unique():
@@ -33,46 +102,36 @@ for jr in abs_df["journal"].unique():
         otr_df = abs_df.loc[(abs_df["journal"] != jr) & (abs_df["year_range"] != yr)]
         wc = ptr_df["word_count"].mean()
         _, pv = stats.ttest_ind(
-            ptr_df["word_count"],
-            otr_df["word_count"],
-            equal_var=False
+            ptr_df["word_count"], otr_df["word_count"], equal_var=False
         )
         x_df = DataFrame(
             {"year_range": yr, "journal": jr, "word_count": wc, "p_value": pv},
-            index=[0]
+            index=[0],
         )
         bt_df = concat([bt_df, x_df], ignore_index=True)
 
-yr_tot_df = DataFrame(columns=["year_range","journal","word_count","p_value"])
+yr_tot_df = DataFrame(columns=["year_range", "journal", "word_count", "p_value"])
 for yr in abs_df["year_range"].unique():
     ptr_df = abs_df.loc[abs_df["year_range"] == yr]
     otr_df = abs_df.loc[abs_df["year_range"] != yr]
     wc = ptr_df["word_count"].mean()
-    _, pv = stats.ttest_ind(
-        ptr_df["word_count"],
-        otr_df["word_count"],
-        equal_var=False
-    )
+    _, pv = stats.ttest_ind(ptr_df["word_count"], otr_df["word_count"], equal_var=False)
     x_df = DataFrame(
         {"year_range": yr, "journal": "Total", "word_count": wc, "p_value": pv},
-        index=[0]
+        index=[0],
     )
     yr_tot_df = concat([yr_tot_df, x_df], ignore_index=True)
 bt_df = concat([bt_df, yr_tot_df], ignore_index=True)
 
-jr_tot_df = DataFrame(columns=["year_range","journal","word_count","p_value"])
+jr_tot_df = DataFrame(columns=["year_range", "journal", "word_count", "p_value"])
 for jr in abs_df["journal"].unique():
     ptr_df = abs_df.loc[abs_df["journal"] == jr]
     otr_df = abs_df.loc[abs_df["journal"] != jr]
     wc = ptr_df["word_count"].mean()
-    _, pv = stats.ttest_ind(
-        ptr_df["word_count"],
-        otr_df["word_count"],
-        equal_var=False
-    )
+    _, pv = stats.ttest_ind(ptr_df["word_count"], otr_df["word_count"], equal_var=False)
     x_df = DataFrame(
         {"year_range": "Total", "journal": jr, "word_count": wc, "p_value": pv},
-        index=[0]
+        index=[0],
     )
     jr_tot_df = concat([jr_tot_df, x_df], ignore_index=True)
 bt_df = concat([bt_df, jr_tot_df], ignore_index=True)
@@ -80,26 +139,19 @@ bt_df = concat([bt_df, jr_tot_df], ignore_index=True)
 ptr_df = abs_df
 otr_df = abs_df
 wc = ptr_df["word_count"].mean()
-_, pv = stats.ttest_ind(
-    ptr_df["word_count"],
-    otr_df["word_count"],
-    equal_var=False
-)
+_, pv = stats.ttest_ind(ptr_df["word_count"], otr_df["word_count"], equal_var=False)
 tot_df = DataFrame(
     {"year_range": "Total", "journal": "Total", "word_count": wc, "p_value": pv},
-    index=[0]
+    index=[0],
 )
 bt_df = concat([bt_df, tot_df], ignore_index=True)
 
 bt_pivot = bt_df.pivot_table(
-    index="year_range",
-    columns="journal",
-    values=["word_count","p_value"]
+    index="year_range", columns="journal", values=["word_count", "p_value"]
 )
 bt_pivot = bt_pivot.round(4)
 bt_pivot.columns = [f"{k} {j}" for j, k in bt_pivot.columns]
-# bt_pivot = bt_pivot.reindex(sorted(bt_pivot.columns), axis=1)
-journals = sorted(bt_df['journal'].unique())
-column_order = [f"{j} {col}" for j in journals for col in ['word_count', 'p_value']]
+journals = sorted(bt_df["journal"].unique())
+column_order = [f"{j} {col}" for j in journals for col in ["word_count", "p_value"]]
 bt_pivot = bt_pivot.reindex(columns=column_order)
-bt_pivot.to_csv(tbl_dir + "statistics-pivot.csv")
+bt_pivot.to_csv(tbl_dir + "statistics-pivot-all.csv")
--- Scrap/abstracts-analysis.py
+++ Scrap/abstracts-analysis.py
@@ -1,14 +1,29 @@
-from pandas import read_csv
+from pandas import read_csv, set_option
 from os import getcwd
+
+# set_option('display.width', None)
 
 dir = getcwd()
 data_dir = dir.replace("Scrap", "Data\\")
 abs_df = read_csv(data_dir + "abstracts.csv")
-abs_df = abs_df[abs_df["segment"] == 1]
+yr_df_sample = read_csv(data_dir + "year-sample.csv")
 
-counts = abs_df['year_range'].value_counts().reset_index()
-counts.columns = ['year_range', 'count']
-print(counts)
 
-# counts = abs_df.groupby(['journal', 'year_range']).size().reset_index(name='count')
-# counts.to_csv("journal-year-counts.csv", index=False)
+seg_df_sample = abs_df.merge(
+    yr_df_sample[["article_url", "year_range", "url"]],
+    on=["article_url", "year_range", "url"],
+    how="inner",
+)
+
+print("Instances in year-sample.csv: ", len(yr_df_sample))
+print("Instances in segment sample: ", len(seg_df_sample))
+print("Missing instances: ", abs((len(yr_df_sample) * 3) - len(seg_df_sample)))
+print("")
+
+# Create a boolean mask that identifies the duplicate rows
+duplicate_mask = seg_df_sample.duplicated(subset=['article_url', 'year_range', 'url', 'segment'])
+
+# Index the dataframe using the duplicate_mask to print out the duplicate rows
+duplicate_rows = seg_df_sample[duplicate_mask]
+print(len(duplicate_rows))
+print(duplicate_rows)
