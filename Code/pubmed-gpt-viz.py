b02d8d037b829c4f7cafffd90e473dc69f4dd872
Tom Gorbett <gorbetw@ccf.org>
2023-02-24 12:59:04 -0500

    fixing index issue with legend replacement updates to paper

--- Code/pubmed-gpt-viz.py
+++ Code/pubmed-gpt-viz.py
@@ -49,7 +49,7 @@ new_order = [
 ]
 yr_grouped = yr_grouped.reindex(columns=new_order)
 
-tick_labels = yr_grouped["year_range"].astype(str)
+tick_labels = yr_grouped.index.astype(str)
 yr_grouped.plot(kind="bar", stacked=True, color=colors, figsize=(20, 10))
 
 plt.xlabel("Year Range")
@@ -58,29 +58,29 @@ plt.ylabel("Number of Abstracts")
 plt.title("Real published extracts evaluated by AI generated text detector")
 plt.savefig(viz_dir + "Years.png")
 
-# by year, 3d
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
+# # by year, 3d
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection="3d")
 
-ax.set_xlabel("Year Range")
-ax.set_xticklabels(yr_grouped["year_range"])
-ax.set_zlabel("Abstracts")
-ax.set_ylim3d(0,10)
+# ax.set_xlabel("Year Range")
+# ax.set_xticklabels(yr_grouped["year_range"])
+# ax.set_zlabel("Abstracts")
+# ax.set_ylim3d(0,10)
 
-dz = zeros(yr_grouped.shape[0])
-for i in range(yr_grouped.shape[1]-1):
-    ax.bar3d(
-        yr_grouped.index, # starting point for x
-        1, # starting point for y
-        dz, # starting point for z
-        0.5, # width of bar
-        0.5, # depth of bar
-        yr_grouped.iloc[:, i+1], # height of bar
-        color=colors[i]
-    )
-    dz += yr_grouped.iloc[:, i+1]
+# dz = zeros(yr_grouped.shape[0])
+# for i in range(yr_grouped.shape[1]-1):
+#     ax.bar3d(
+#         yr_grouped.index, # starting point for x
+#         1, # starting point for y
+#         dz, # starting point for z
+#         0.5, # width of bar
+#         0.5, # depth of bar
+#         yr_grouped.iloc[:, i+1], # height of bar
+#         color=colors[i]
+#     )
+#     dz += yr_grouped.iloc[:, i+1]
 
-plt.savefig(viz_dir + "Years-3D.png")
+# plt.savefig(viz_dir + "Years-3D.png")
 
 # by journal
 jr_grouped = yr_df_sample.groupby(["journal", "Legend"]).size().unstack()
@@ -95,7 +95,7 @@ new_order = [
 ]
 
 yr_grouped = yr_grouped.reindex(columns=new_order)
-tick_labels = jr_grouped["journal"].astype(str)
+tick_labels = jr_grouped.index.astype(str)
 jr_grouped.plot(kind="bar", stacked=True, color=colors, figsize=(20, 10))
 
 plt.xlabel("Journal")
@@ -104,29 +104,29 @@ plt.ylabel("Number of Abstracts")
 plt.title("Real published extracts evaluated by AI generated text detector")
 plt.savefig(viz_dir + "Journals.png")
 
-# by journal, 3d
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
+# # by journal, 3d
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection="3d")
 
-ax.set_xlabel("Journal")
-ax.set_xticklabels(jr_grouped["journal"])
-ax.set_zlabel("Abstracts")
-ax.set_ylim3d(0,10)
+# ax.set_xlabel("Journal")
+# ax.set_xticklabels(jr_grouped["journal"])
+# ax.set_zlabel("Abstracts")
+# ax.set_ylim3d(0,10)
 
-dz = zeros(jr_grouped.shape[0])
-for i in range(jr_grouped.shape[1]-1):
-    ax.bar3d(
-        jr_grouped.index, # starting point for x
-        1, # starting point for y
-        dz, # starting point for z
-        0.5, # width of bar
-        0.5, # depth of bar
-        jr_grouped.iloc[:, i+1], # height of bar
-        color=colors[i]
-    )
-    dz += jr_grouped.iloc[:, i+1]
+# dz = zeros(jr_grouped.shape[0])
+# for i in range(jr_grouped.shape[1]-1):
+#     ax.bar3d(
+#         jr_grouped.index, # starting point for x
+#         1, # starting point for y
+#         dz, # starting point for z
+#         0.5, # width of bar
+#         0.5, # depth of bar
+#         jr_grouped.iloc[:, i+1], # height of bar
+#         color=colors[i]
+#     )
+#     dz += jr_grouped.iloc[:, i+1]
 
-plt.savefig(viz_dir + "Journals-3D.png")
+# plt.savefig(viz_dir + "Journals-3D.png")
 
 # by segment
 seg_df_sample = abs_df.merge(
@@ -147,26 +147,26 @@ plt.ylabel("Number of Abstracts")
 plt.title("Real published extracts evaluated by AI generated text detector")
 plt.savefig(viz_dir + "Segments.png")
 
-# by segment, 3d
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
+# # by segment, 3d
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection="3d")
 
-ax.set_xlabel("Segment")
-ax.set_xticklabels(seg_grouped["segment"])
-ax.set_zlabel("Abstracts")
-ax.set_ylim3d(0,10)
+# ax.set_xlabel("Segment")
+# ax.set_xticklabels(seg_grouped["segment"])
+# ax.set_zlabel("Abstracts")
+# ax.set_ylim3d(0,10)
 
-dz = zeros(seg_grouped.shape[0])
-for i in range(seg_grouped.shape[1]-1):
-    ax.bar3d(
-        seg_grouped.index, # starting point for x
-        1, # starting point for y
-        dz, # starting point for z
-        0.5, # width of bar
-        0.5, # depth of bar
-        seg_grouped.iloc[:, i+1], # height of bar
-        color=colors[i]
-    )
-    dz += seg_grouped.iloc[:, i+1]
+# dz = zeros(seg_grouped.shape[0])
+# for i in range(seg_grouped.shape[1]-1):
+#     ax.bar3d(
+#         seg_grouped.index, # starting point for x
+#         1, # starting point for y
+#         dz, # starting point for z
+#         0.5, # width of bar
+#         0.5, # depth of bar
+#         seg_grouped.iloc[:, i+1], # height of bar
+#         color=colors[i]
+#     )
+#     dz += seg_grouped.iloc[:, i+1]
 
-plt.savefig(viz_dir + "Segments-3D.png")
+# plt.savefig(viz_dir + "Segments-3D.png")
